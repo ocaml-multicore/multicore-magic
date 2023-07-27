a96ad866e2e01b09564c247643971a948c3edcc5
Vesa Karvonen <vesa.a.j.k@gmail.com>
2023-07-27 17:49:35 +0300

    Prefer putting inline attribute immediately after `let`

--- src/Multicore_magic.ml
+++ src/Multicore_magic.ml
@@ -13,16 +13,14 @@ let make_padded_array n x =
   if x != Obj.magic () then Array.fill a 0 n x;
   a
 
-let length_of_padded_array x = Array.length x - num_padding_words [@@inline]
+let[@inline] length_of_padded_array x = Array.length x - num_padding_words
 
-let length_of_padded_array_minus_1 x = Array.length x - (num_padding_words + 1)
-[@@inline]
+let[@inline] length_of_padded_array_minus_1 x =
+  Array.length x - (num_padding_words + 1)
 
-let fenceless_get (atomic : 'a Atomic.t) = !(Obj.magic atomic : 'a ref)
-[@@inline]
+let[@inline] fenceless_get (atomic : 'a Atomic.t) = !(Obj.magic atomic : 'a ref)
 
-let fenceless_set (atomic : 'a Atomic.t) value =
+let[@inline] fenceless_set (atomic : 'a Atomic.t) value =
   (Obj.magic atomic : 'a ref) := value
-[@@inline]
 
-let fence atomic = Atomic.fetch_and_add atomic 0 |> ignore [@@inline]
+let[@inline] fence atomic = Atomic.fetch_and_add atomic 0 |> ignore
